1b5e4eb795bc20ba07e876105f5cddbafd5f6247
imkrsnna <krishna.singh@bbd.co.za>
2024-02-19 01:48:50 +0530

    inserting dummy data

--- migrations/V4__insertingdummydata_table.sql
+++ migrations/V4__insertingdummydata_table.sql
@@ -1,4 +1,4 @@
 INSERT INTO UserAccounts (FirstName, LastName, Username, ImageURL, EmailAddress, ProfileDescription, IsActive)
-VALUES ('John', 'Doe', 'john.doe', 'image_url1.jpg', 'john.doe@email.com', 'Profile description for John', 1),
-       ('Jane', 'Smith', 'jane.smith', 'image_url2.jpg', 'jane.smith@email.com', 'Profile description for Jane', 1);
+VALUES ('John', 'Doe', 'hbhjj', 'image_url1.jpg', 'gygjh.doe@email.com', 'Profile description for John', 1),
+       ('Jane', 'Smith', 'janehvhnn', 'image_url2.jpg', 'hgvgh.smith@email.com', 'Profile description for Jane', 1);
 
