365d20aa08a65402dafce529a3a5cdbd9dc0f10b
imkrsnna <krishna.singh@bbd.co.za>
2024-02-18 16:32:17 +0530

    Remove V21__insert_user.sql file

--- migrations/V21__insert_user.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Users (UserID, FirstName, LastName, Username, ImageUrl, Email, ProfileDescription, ActiveStatus)
-VALUES 
-    (13, 'John', 'Doe', 'johndoe', 'url_to_image1.jpg', 'john.doe@email.com', 'A passionate individual.', 1);
