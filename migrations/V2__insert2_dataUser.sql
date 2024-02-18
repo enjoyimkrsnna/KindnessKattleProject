0adce11d7d6efe4c7e82cdb1580e8fa06f307e5a
enjoyimkrsnna <160381485+enjoyimkrsnna@users.noreply.github.com>
2024-02-18 19:51:12 +0530

    Delete migrations directory

--- migrations/V2__insert2_dataUser.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Users (UserID, FirstName, LastName, Username, ImageUrl, Email, ProfileDescription, ActiveStatus)
-VALUES 
-    (77, 'John', 'Doe', 'johndoe', 'url_to_image1.jpg', 'john.doe@email.com', 'A passionate individual.', 1),
-    (55, 'Jane', 'Smith', 'janesmith', 'url_to_image2.jpg', 'jane.smith@email.com', 'Food lover and enthusiast.', 1)
