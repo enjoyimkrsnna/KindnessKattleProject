c29936f5721f1196e39ac2be3c743da2fd0a8357
imkrsnna <krishna.singh@bbd.co.za>
2024-02-18 15:52:50 +0530

    add files

---
+++ migrations/V21__insert_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO Users (UserID, FirstName, LastName, Username, ImageUrl, Email, ProfileDescription, ActiveStatus)
+VALUES 
+    (8, 'John', 'Doe', 'johndoe', 'url_to_image1.jpg', 'john.doe@email.com', 'A passionate individual.', 1);
