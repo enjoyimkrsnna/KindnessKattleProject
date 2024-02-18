ff93fdc8c808f0738f89c4bf8dcbdbb8aa8c7552
Ajay Pratap Singh <116877279+apsinghrana100@users.noreply.github.com>
2024-02-18 17:37:12 +0530

    ajay_commited
    this is file

---
+++ migrations/V2__create_abc_insertad.sql
@@ -0,0 +1,4 @@
+INSERT INTO Users (UserID, FirstName, LastName, Username, ImageUrl, Email, ProfileDescription, ActiveStatus)
+VALUES 
+    (909, 'John', 'Doe', 'johndoe', 'url_to_image1.jpg', 'john.doe@email.com', 'A passionate individual.', 1),
+    (808, 'Jane', 'Smith', 'janesmith', 'url_to_image2.jpg', 'jane.smith@email.com', 'Food lover and enthusiast.', 1)
