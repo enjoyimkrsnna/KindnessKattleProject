95659e9c496b9511170edf91f4ec06314a7ba07f
apsinghrana100 <apsinghrana100@gmail.com>
2024-02-19 01:55:29 +0530

    insert data

--- migrations/V14__insert_data.sql
+++ migrations/V14__insert_data.sql
@@ -1,3 +1,3 @@
 INSERT INTO UserAccounts (FirstName, LastName, Username, ImageURL, EmailAddress, ProfileDescription, IsActive)
-VALUES ('Ajay', 'Doe', 'ajay.doe', 'image_url1.jpg', 'ajay.doe@email.com', 'Profile description for John', 1),
-       ('sinhh', 'Smith', 'singh.smith', 'image_url2.jpg', 'singh.smith@email.com', 'Profile description for Jane', 1);
+VALUES ('Ajay', 'Doe', 'vijay.doe', 'image_url1.jpg', 'vijay.doe@email.com', 'Profile description for John', 1),
+       ('sinhh', 'Smith', 'sh.smith', 'image_url2.jpg', 'vijay.smith@email.com', 'Profile description for Jane', 1);
