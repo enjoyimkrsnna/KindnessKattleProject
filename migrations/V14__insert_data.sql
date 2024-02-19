fcc592fbef434603e85f72914b23bd5f04bb4ae1
enjoyimkrsnna <160381485+enjoyimkrsnna@users.noreply.github.com>
2024-02-19 10:58:11 +0530

    Delete migrations/V14__insert_data.sql
    removing unnecessary files

--- migrations/V14__insert_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO UserAccounts (FirstName, LastName, Username, ImageURL, EmailAddress, ProfileDescription, IsActive)
-VALUES ('Ajay', 'Doe', 'vijay.doe', 'image_url1.jpg', 'vijay.doe@email.com', 'Profile description for John', 1),
-       ('sinhh', 'Smith', 'sh.smith', 'image_url2.jpg', 'vijay.smith@email.com', 'Profile description for Jane', 1);
