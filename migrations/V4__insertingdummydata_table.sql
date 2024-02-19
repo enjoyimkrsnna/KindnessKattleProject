28bb6dfb2edf3f0fc07c7a0ecca6ca84e54c4c2f
enjoyimkrsnna <160381485+enjoyimkrsnna@users.noreply.github.com>
2024-02-19 11:03:37 +0530

    Delete migrations/V4__insertingdummydata_table.sql
    removing unnecessary files

--- migrations/V4__insertingdummydata_table.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO UserAccounts (FirstName, LastName, Username, ImageURL, EmailAddress, ProfileDescription, IsActive)
-VALUES ('John', 'Doe', 'hbhjj', 'image_url1.jpg', 'gygjh.doe@email.com', 'Profile description for John', 1),
-       ('Jane', 'Smith', 'janehvhnn', 'image_url2.jpg', 'hgvgh.smith@email.com', 'Profile description for Jane', 1);
-
