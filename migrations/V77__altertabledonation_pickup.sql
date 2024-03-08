748232c8039b041a190d65feec3c7f6a1068f01f
enjoyimkrsnna <160381485+enjoyimkrsnna@users.noreply.github.com>
2024-03-08 16:05:26 +0530

    Delete migrations/V77__altertabledonation_pickup.sql

--- migrations/V77__altertabledonation_pickup.sql
+++
@@ -1,7 +0,0 @@
--- Add PickupInProgress column to DonationPosts table
-ALTER TABLE DonationPosts
-ADD PickupInProgress BIT NOT NULL DEFAULT 0;
-
--- Add PickupCompleted column to PickupStatuses table
-ALTER TABLE PickupStatuses
-ADD PickupCompleted BIT NOT NULL DEFAULT 0;
