fa875ee636ecb24e44f04b13ac16ef0eb66776c7
imkrsnna <krishna.singh@bbd.co.za>
2024-03-08 15:33:18 +0530

    updating database table script

--- migrations/V78__updated_createTableScript.sql
+++ migrations/V78__updated_createTableScript.sql
@@ -34,7 +34,7 @@ CREATE TABLE food_types (
   food_type VARCHAR(20) NOT NULL
 );
  
-CREATE TABLE addresses (
+CREATE TABLE address (
   address_id INT PRIMARY KEY IDENTITY(1, 1),
   address_line NVARCHAR(255) NOT NULL,
   city_id INT NOT NULL,
